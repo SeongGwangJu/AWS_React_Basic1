f3b24d8ae1cf21d588749634ec1357bab566d50b
SeongGwangJu <seonggwangdev@gmail.com>
2023-08-16 13:51:38 +0900

    index.js와 VirtualDom에 대해 학습한 내용

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from 'react'; //node_modules에 설치되어있음
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 가상
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
